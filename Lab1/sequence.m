6322320820d27d5b0036b285c4524152683d2c36
Taylor Artunian <tartunian@gmail.com>
2020-02-03 17:44:32 -0800

    Used Prof. Aboli's code for padData

--- Lab1/sequence.m
+++ Lab1/sequence.m
@@ -5,45 +5,55 @@ classdef sequence
     end
 	
     methods(Static)
-        % Pads the input sequences so that they are of the same length.
-        % Sequence with the lower offset will not have front padding. This
-        % returns the data portion of the sequences only.
+        
         function [a,b] = padData(x,y)
-            % Find which sequencce has the lower offset (furthest to the
-            % left).
-            lo = sequence([],0);
-            hi = sequence([],0);
-            if(x.offset<y.offset)
-                lo = x;
-                hi = y;
-            else
-                lo = y;
-                hi = x;
-            end
-            % Define ints for left and right padding of zeros.
-            leftPad = hi.offset-lo.offset;
-            rightPad = (length(lo.data)+lo.offset)-(length(hi.data)+hi.offset);
-            % Padding the left side of the sequence with the higher offset
-            % is easiest.
-            hi.data = [zeros(1,leftPad),hi.data];
-            % Pad the right side of either the lower or higher offset
-            % sequence depending on whether rightPad is
-            % positive or negative.
-            if(rightPad>0)
-                hi.data = [hi.data, zeros(1,rightPad)];
-            elseif(rightPad<0)
-                lo.data = [lo.data, zeros(1,abs(rightPad))];
-            end
-            % Map lo and hi back to the order in which they came i.e. a = x
-            % and b = y.
-            if(x.offset<y.offset)
-                a=lo.data;
-                b=hi.data;
-            else
-                a=hi.data;
-                b=lo.data;
-            end
+            Lx = length(x.data) + x.offset;
+            Ly = length(y.data) + y.offset;
+            a = [zeros(1,x.offset-y.offset), x.data, zeros(1,Ly-Lx)];
+            b = [zeros(1,y.offset-x.offset), y.data, zeros(1,Lx-Ly)];
         end
+
+%         % My Original Implementation
+%         % 
+%         % Pads the input sequences so that they are of the same length.
+%         % Sequence with the lower offset will not have front padding. This
+%         % returns the data portion of the sequences only.
+%         function [a,b] = padData(x,y)
+%             % Find which sequencce has the lower offset (furthest to the
+%             % left).
+%             lo = sequence([],0);
+%             hi = sequence([],0);
+%             if(x.offset<y.offset)
+%                 lo = x;
+%                 hi = y;
+%             else
+%                 lo = y;
+%                 hi = x;
+%             end
+%             % Define ints for left and right padding of zeros.
+%             leftPad = hi.offset-lo.offset;
+%             rightPad = (length(lo.data)+lo.offset)-(length(hi.data)+hi.offset);
+%             % Padding the left side of the sequence with the higher offset
+%             % is easiest.
+%             hi.data = [zeros(1,leftPad),hi.data];
+%             % Pad the right side of either the lower or higher offset
+%             % sequence depending on whether rightPad is
+%             % positive or negative.
+%             if(rightPad>0)
+%                 hi.data = [hi.data, zeros(1,rightPad)];
+%             elseif(rightPad<0)
+%                 lo.data = [lo.data, zeros(1,abs(rightPad))];
+%             end
+%             % Map lo and hi back to the order in which they came i.e. a = x
+%             % and b = y.
+%             if(x.offset<y.offset)
+%                 a=lo.data;
+%                 b=hi.data;
+%             else
+%                 a=hi.data;
+%                 b=lo.data;
+%             end
+%         end
     end
     
 	methods
@@ -93,6 +103,7 @@ classdef sequence
                 [a, b] = sequence.padData(x,y);
                 z = sequence(a+b,min(x.offset,y.offset));
             end
+            %trim(z);
         end
 		
 		function z = minus(x, y)
@@ -104,6 +115,7 @@ classdef sequence
                 [a, b] = sequence.padData(x,y);
                 z = sequence(a-b,min(x.offset,y.offset));
             end
+            %trim(z);
 		end
 		
 		function z = times(x, y)
@@ -115,7 +127,17 @@ classdef sequence
                 [a, b] = sequence.padData(x,y);
                 z = sequence(a.*b,min(x.offset,y.offset));
             end
-		end
+            %trim(z);
+        end
+        
+        function x = trim(x)
+            while(x.data(1) == 0 && length(x.data)>1)
+                x.data(1) = [];
+            end
+            while(x.data(end) == 0 && length(x.data)>1)
+                x.data(end) = [];
+            end
+        end
 		
 		function stem(x)
 			% STEM Display a Matlab sequence, x, using a stem plot.
