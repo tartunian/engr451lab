8ceb221023e65372bfd2a521e3fd1a8f816df49c
Taylor Artunian <tartunian@sfsu.edu>
2020-03-02 17:15:58 -0800

    Added Lab 4 DTFT

--- SequenceLabs/sequence.m
+++ SequenceLabs/sequence.m
@@ -130,6 +130,50 @@ classdef sequence
             %trim(z);
         end
         
+        function y = even(x)
+            data = 0.5*(x.data+conj(x.flip().data))
+            y = sequence(data,x.offset);
+        end
+        
+        function y = odd(x)
+            data = 0.5*(x.data-conj(x.flip().data));
+            y = sequence(data,x.offset);
+        end
+        
+        function y = conj(x)
+            y = conj(x)
+        end
+        
+        % DTFT
+        % Periodic result between -pi pi or -2pi 2pi
+        % Real valued: magnitude response = even function
+        % Example: x[n] = dirac[n+3]+dirac[n+2]+dirac[n+1]
+        % X(e^jw) = e^3jw + e^2jw + e^jw = e^2jw(e^jw + 1 + e^-jw) = e^2jw(1+2cos(w))
+        % w is a vector from -pi to pi , not a variable
+        % A = exp(-j*Q) answer
+        
+        function y = dtft(x,w)
+            lx = length(x.data);
+            n = linspace(x.offset,x.offset+lx-1,lx);
+            Q = n'*w
+            y = exp(-j*Q); % Does this need to be cos(wn) + j sin(wn)
+        end
+        
+        % DTFT2 Find real and imaginary parts of DTFT
+        
+        function y = dtft2(x,w)
+            Q = dtft(x,w);
+            lx = length(x);
+            lw = length(w);
+            for i = 1 : lx
+                for j = 1 : lw
+                    Q(i,j)
+                end
+            end
+            y.real = real(Q);
+            y.imag = imag(Q);
+        end
+        
         function y = conv(x,h)
             lx = length(x.data);
             lh = length(h.data);
